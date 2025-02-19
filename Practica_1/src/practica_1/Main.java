048188d24c61e5041d283982935773e5d9460f04
HerlanFabioQuispePari <hquispep@fcpn.edu.bo>
2025-02-19 06:38:46 -0400

    Add files via upload
    Herlan Fabio Quispe Pari

---
+++ Practica_1/src/practica_1/Main.java
@@ -0,0 +1,13 @@
+
+package practica_1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Punto p1 = new Punto(2,3);
+        System.out.println(p1.coordCartesianas());
+        
+        p1.coord_polares();
+    }
+    
+}
---
+++ Practica_1/src/practica_1/Punto.java
@@ -0,0 +1,44 @@
+
+package practica_1;
+
+public class Punto {
+    public float x;
+    public float y;
+
+    public Punto(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+    
+    public Punto coordCartesianas() {
+        return new Punto(getX(), getY());
+    }
+    
+    public void coord_polares(){
+        float radio = (float) Math.sqrt(x * x + y * y);
+        float angulo = (float) Math.toDegrees(Math.atan2(y, x));
+        System.out.println("radio: "+ radio);
+        System.out.println("angulo: "+angulo);
+    }
+    
+    @Override
+    public String toString() {
+        return "x: "+x+" y: "+y;
+    }
+}
